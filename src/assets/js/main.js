0e86c1f3d7fd5e3a8ef30d9a964222144180c69b
AlexIstra1989 <alex888fcsm@gmail.com>
2023-03-14 23:02:38 +0300

    added heading block

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,16 @@
+//Burger
 $('.header__btn').on('click', function() {
  $('.menu').toggleClass('menu_active'),
  $('.header__btn').toggleClass('header__btn_active');
+});
+
+//Sticky header
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 0) { /* если страница прокручена хотя бы на 1 пиксель */
+    header.classList.add('scrolled'); /* добавляем класс, задающий новый фон */
+  } else {
+    header.classList.remove('scrolled'); /* удаляем класс, возвращающий прозрачный фон */
+  }
 });
